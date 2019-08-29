2ca26d5183cd8587e68041c9e7fd4010012bafb0
abhishekyana <abhishekasdzxc@gmail.com>
2019-08-29 15:59:59 +0530

    Added

--- deploy.py
+++ deploy.py
@@ -1,7 +1,16 @@
-from flask import Flask 
-
+import numpy as np
+from flask import Flask
+from flask import jsonify
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return "<h1>Deeployed to Heorku!!! Abhishek Yanamandra</h1>"
+
+
+@app.route('/random', methods=['GET'])
+def random():
+    size = request.args.get('size')
+    dims = [int(k) for k in size.split('x')]
+    return jsonify(array=np.random.randn(*dims).tolist(), size=int(np.array(dims).prod()))
