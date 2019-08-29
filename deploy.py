096025980ff7bce246f4e7e1e60671b72815bc93
abhishekyana <abhishekasdzxc@gmail.com>
2019-08-29 16:27:08 +0530

    Added

--- deploy.py
+++ deploy.py
@@ -2,6 +2,7 @@ import numpy as np
 from flask import Flask
 from flask import jsonify
 from flask import request
+import torch
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,3 +15,13 @@ def random():
     size = request.args.get('size')
     dims = [int(k) for k in size.split('x')]
     return jsonify(array=np.random.randn(*dims).tolist(), size=int(np.array(dims).prod()))
+
+
+model = torch.nn.Linear(100,20)
+
+@app.route('/nn', methods=['GET'])
+def nn():
+	inData = torch.randn(100)
+	outData = model(inData)
+    return jsonify(classes=outData.detach().numpy())
+
